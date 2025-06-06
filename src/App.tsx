742c44447e3b88c73907a2833361eb4c66528d9a
Kuldeep Kumar <kuldeepkhatri03@gmail.com>
2025-06-06 20:03:53 +0500

    added the Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-    <Grid temp>
-      <GridItem>
-    </Grid>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <GridItem display={{base:"none",lg:"block"}} area="aside" bg="yellow.300">
+          Aside
+        </GridItem>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
